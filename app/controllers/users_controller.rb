f9ca5ceecad13e664511bb3cb475a589efe44e59
Dustin Lewis <d.lew23@gmail.com>
2014-09-24 21:50:01 -0700

    complete users controller and model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,51 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = User.all
+  end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "That's a new user you just created!"
+      redirect_to '/users'
+    else
+      render 'new'
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "#{@user.name} updated successfully, bro!"
+      redirect_to '/users'
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:notice] = "That user is now dead."
+    redirect_to '/users'
+  end
+
+private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 
 end
