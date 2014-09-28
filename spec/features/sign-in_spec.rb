af892eaa6cd573ae88a6833ca5778e430745ac0a
Dustin Lewis <d.lew23@gmail.com>
2014-09-27 17:28:38 -0700

    pass failed login spec

--- spec/features/sign-in_spec.rb
+++ spec/features/sign-in_spec.rb
@@ -7,4 +7,13 @@ describe "the sign-in process" do
     click_button 'Log In'
     expect(page).to have_content "It's Bobby, everybody!"
   end
+
+  it "gives user an error when they use the wrong password" do
+    visit 'sessions/new'
+    user = User.create(:name => 'Bobby', :email => 'user@example.com', :password => 'password')
+    fill_in 'Email', :with => 'user@example.com'
+    fill_in 'Password', :with => 'wrong'
+    click_button 'Log In'
+    page.should have_content "Email or password is invalid"
+  end
 end
