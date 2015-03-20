5de30c3aab7414e087e727cadce5fa8684d76c3b
Dustin Lewis <d.lew23@gmail.com>
2015-03-20 13:15:04 -0700

    add uniqueness of artist name

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,5 +1,6 @@
 class Artist < ActiveRecord::Base
-  validates :name, :presence => :true
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+
   has_many :shows, dependent: :destroy
 
 end
